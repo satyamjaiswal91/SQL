eee247cb49cd861d3e8b6ea6f049ae79ec0e62b1
satyamjaiswal91 <118043806+satyamjaiswal91@users.noreply.github.com>
2022-11-25 22:59:31 +0530

    Day3 homework

---
+++ Day3Crud.sql
@@ -0,0 +1,7 @@
+use hello_world;
+Create table countries(
+S_no int primary key,
+stateName varchar(20) not null,
+No_of_District int not null);
+insert into countries values(1,'Jammu and Kashmir',21);
+Select * from countries;
